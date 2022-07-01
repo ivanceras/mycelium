ce3c94563d83fdd15b55770cc0d79f27158cc0db
Jovansonlee Cesar <ivanceras@gmail.com>
2022-07-01 10:32:46 +0800

    feat: add a pallet post where it keeps track of posted content by authors

---
+++ substrate-node-template/pallets/post/src/tests.rs
@@ -0,0 +1,15 @@
+use crate::{mock::*, Error};
+use frame_support::BoundedSlice;
+use frame_support::BoundedVec;
+use frame_support::{assert_noop, assert_ok};
+
+#[test]
+fn it_works_for_default_value() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		let content = BoundedVec::try_from(b"hello".to_vec()).unwrap();
+		assert_ok!(TemplateModule::post_content(Origin::signed(1), content));
+		// Read pallet storage and assert an expected result.
+		assert_eq!(TemplateModule::total(), 1);
+	});
+}
