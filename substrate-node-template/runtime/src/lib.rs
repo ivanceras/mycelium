febb0776a822a58639c0e2cf76dae8baf78d53ee
Jovansonlee Cesar <ivanceras@gmail.com>
2022-07-01 18:53:22 +0800

    feat: add pallet-forum as ForumModule into the substrate runtime

--- substrate-node-template/runtime/src/lib.rs
+++ substrate-node-template/runtime/src/lib.rs
@@ -46,6 +46,9 @@ pub use sp_runtime::{Perbill, Permill};
 /// Import the template pallet.
 pub use pallet_template;
 
+/// import the forum pallet.
+pub use pallet_forum;
+
 /// An index to a block.
 pub type BlockNumber = u32;
 
@@ -266,6 +269,11 @@ impl pallet_template::Config for Runtime {
 	type Event = Event;
 }
 
+impl pallet_forum::Config for Runtime {
+	type Event = Event;
+	type MaxLength = ConstU32<280>;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -283,6 +291,7 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		ForumModule: pallet_forum,
 	}
 );
 
