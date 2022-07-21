a8ab16de089a57f70b07c67fc36cce4f5abc47c8
Jovansonlee Cesar <ivanceras@gmail.com>
2022-07-21 17:31:13 +0800

    fix: example

--- examples/show_storage_value.rs
+++ examples/show_storage_value.rs
@@ -4,7 +4,6 @@ use mycelium::sp_core::crypto::AccountId32;
 use mycelium::Api;
 use pallet_balances::AccountData;
 use sp_keyring::AccountKeyring;
-use scale_info::TypeDef
 
 #[tokio::main]
 async fn main() -> Result<(), mycelium::Error> {
